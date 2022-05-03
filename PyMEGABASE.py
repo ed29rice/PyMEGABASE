c3ae5de5484cad9eb2a3fd22121610bdec9a124f
esdoro <dodero051295@gmail.com>
2022-05-03 16:40:13 -0500

    removed some prints

--- PyMEGABASE.py
+++ PyMEGABASE.py
@@ -376,11 +376,9 @@ class PyMEGABASE:
         #Add gaps from UCSC database
         gaps=np.loadtxt('PyMEGABASE/centromeres/'+self.assembly+'_gaps.txt',dtype=str)
         chr_gaps_ndx=np.where((gaps[:,0]=='chr'+str(chr)))[0]
-        print(chr_gaps_ndx,chr)
         for gp in chr_gaps_ndx:
             init_loci=np.round(gaps[gp,1].astype(float)/50000).astype(int)
             end_loci=np.round(gaps[gp,2].astype(float)/50000).astype(int)
-            print(init_loci,end_loci,gaps[gp])
             predict_type[init_loci:end_loci]=6
      
         return predict_type
@@ -848,11 +846,9 @@ class PyMEGABASE_extended:
         #Add gaps from UCSC database
         gaps=np.loadtxt('PyMEGABASE/centromeres/'+self.assembly+'_gaps.txt',dtype=str)
         chr_gaps_ndx=np.where((gaps[:,0]=='chr'+str(chr)))[0]
-        print(chr_gaps_ndx,chr)
         for gp in chr_gaps_ndx:
             init_loci=np.round(gaps[gp,1].astype(float)/50000).astype(int)
             end_loci=np.round(gaps[gp,2].astype(float)/50000).astype(int)
-            print(init_loci,end_loci,gaps[gp])
             predict_type[init_loci:end_loci]=6
                
         return predict_type
