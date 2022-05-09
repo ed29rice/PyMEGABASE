66a36178fada374e7f985b54d2259fc111e6f858
esdoro <dodero051295@gmail.com>
2022-05-09 12:53:45 -0500

    Nstates processing and regularization options

--- PyMEGABASE.py
+++ PyMEGABASE.py
@@ -1255,14 +1255,14 @@ class PyMEGABASE_z_extended:
                 f.write('>'+str(i).zfill(4)+'\n')
                 f.write(''.join(sequences[:,i])+'\n')    
     
-    def training(self,nproc=10):
+    def training(self,nproc=10,lambda_h=100,lambda_J=100):
         # Compute DCA scores using Pseudolikelihood maximization algorithm
         plmdca_inst = plmdca.PlmDCA(
             self.cell_line_path+"/sequences.fa",
             'protein',
             seqid = 0.99,
-            lambda_h = 100,
-            lambda_J = 100,
+            lambda_h = lambda_h,
+            lambda_J = lambda_J,
             num_threads = nproc,
             max_iterations = 1000)
         print('Training started')
